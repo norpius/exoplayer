29eaf8c6e781e9b2a79c322279a7df3ff3e981d8
Luca Ambrosi <LAmbrosi.consulente@CattolicaAssicurazioni.it>
2019-12-09 17:39:43 +0100

    minimal exoplayer for HLS uri

---
+++ app/src/main/java/com/luca/qvc/MainActivity.java
@@ -0,0 +1,43 @@
+package com.luca.qvc;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String VIDEO_URI = "https://qvcitalia.mmdlive.lldns.net/qvcitalia/66f8359ae9c046bb8aa92144511b450a/chunklist_b2592000.m3u8";
+
+    private SimpleExoPlayer player;
+    private PlayerView playerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.a_main_exoplayer);
+
+        player = ExoPlayerFactory.newSimpleInstance(getApplicationContext());
+        playerView.setPlayer(player);
+
+        DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36");
+        player.setPlayWhenReady(true);
+        player.prepare(new HlsMediaSource.Factory(dataSourceFactory)
+                .setAllowChunklessPreparation(true)
+                .createMediaSource(Uri.parse(VIDEO_URI)));
+    }
+}
